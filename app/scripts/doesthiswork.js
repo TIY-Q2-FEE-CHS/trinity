c9cd5a9bbac95de215f0429a067a32dcdc1c5a8e
Devon Hicks <devon@devonhicks.com>
2014-05-18 13:35:08 -0400

    At some point today, this shall work

--- app/scripts/doesthiswork.js
+++ app/scripts/doesthiswork.js
@@ -1,21 +1,61 @@
-var map;
-function initialize() 
-{ 
-    var mapOptions = {
-    center: new google.maps.LatLng('23.11', '71.00'),
-    zoom: 2,
-    scrollwheel: false,
-    disableDefaultUI: false,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
- };
+function initialize() {
+  var mapOptions = {
+    zoom: 8,
+    center: new google.maps.LatLng(32.7833, -79.9333)
+  };
+  var map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+}
 
-  map = new google.maps.Map(document.getElementById("map_canvas"),mapOptions);
- }
+google.maps.event.addDomListener(window, 'load', initialize);
 
- function addMarker()
-  {
-     marker = new google.maps.Marker({
-                position: new google.maps.LatLng(23.72, 72.100),
-                map: map,
+function setMarkers(center, radius, map) {
+        var json = (function () { 
+            var json = null; 
+            $.ajax({ 
+                'async': false, 
+                'global': false, 
+                'url': "http://tiy-fee-rest.herokuapp.com/collections/trinity", 
+                'dataType': "json", 
+                'success': function (data) {
+                     json = data; 
+                 }
             });
-  }
+            return json;
+        })();
+
+        var circle = new google.maps.Circle({
+                strokeColor: '#000000',
+                strokeOpacity: 0.25,
+                strokeWeight: 1.0,
+                fillColor: '#ffffff',
+                fillOpacity: 0.1,
+                clickable: false,
+                map: map,
+                center: center,
+                radius: radius
+            });
+        var bounds = circle.getBounds();
+
+        //loop between each of the json elements
+        for (var i = 0, length = json.length; i < length; i++) {
+            var data = json[i],
+            latLng = new google.maps.LatLng(data.lat, data.lng); 
+
+
+
+            if(bounds.contains(latLng)) {
+                // Creating a marker and putting it on the map
+                var marker = new google.maps.Marker({
+                    position: latLng,
+                    map: map,
+                    title: data.content
+                });
+                infoBox(map, marker, data);
+            }
+        }
+
+        circle.bindTo('center', marker, 'position');
+    }
+
+     
